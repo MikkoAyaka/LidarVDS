d8c2c5c13ccbe14b807817e3165b4d828418f8f4
MikkoAyaka <3401286177@qq.com>
2023-04-03 21:33:19 +0800

    完成主页面导航栏的开发
    Signed-off-by: MikkoAyaka <3401286177@qq.com>

---
+++ LidarVDS/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HandyControl.Controls;
+
+namespace LidarVDS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : GlowWindow
+    {
+        private static MainWindow INSTANCE;
+        public MainWindow()
+        {
+            INSTANCE = this;
+            InitializeComponent();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void HideWindow(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            WindowState = WindowState.Minimized;
+        }
+        private void CloseWindow(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            Close();
+        }
+    }
+}
