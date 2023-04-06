e2f8971a77ea59b173008e0fa6d7fb358ad4614b
MikkoAyaka <3401286177@qq.com>
2023-04-06 08:58:39 +0800

    完成了 快速使用 按钮的展开栏及动画效果
    Signed-off-by: MikkoAyaka <3401286177@qq.com>

--- LidarVDS/MainWindow.xaml.cs
+++ LidarVDS/MainWindow.xaml.cs
@@ -1,43 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using HandyControl.Controls;
 using Window = HandyControl.Controls.Window;
 
 namespace LidarVDS
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        /**
+         * 菜单初始化
+         */
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        /**
+         * 拖拽窗口
+         */
         private void DragWindow(object sender, MouseButtonEventArgs e)
         {
             DragMove();
         }
 
+        /**
+         * 最小化窗口
+         */
         private void HideWindow(object sender, RoutedEventArgs e)
         {
             e.Handled = true;
             WindowState = WindowState.Minimized;
         }
-
+        
+        /**
+         * 关闭窗口
+         */
         private void CloseWindow(object sender, RoutedEventArgs e)
         {
             e.Handled = true;
