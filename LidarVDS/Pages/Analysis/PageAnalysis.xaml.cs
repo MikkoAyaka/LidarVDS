f5424670da6887ac44e38ac48307420779145d3f
MikkoAyaka <3401286177@qq.com>
2023-04-16 11:03:39 +0800

    完成 Analysis 页面模板
    Signed-off-by: MikkoAyaka <3401286177@qq.com>

---
+++ LidarVDS/Pages/Analysis/PageAnalysis.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows.Controls;
+
+namespace LidarVDS.Pages.Analysis;
+
+public partial class PageAnalysis : Page
+{
+    [Obsolete("请通过相应Service类获取单例实例")]
+    public PageAnalysis()
+    {
+        InitializeComponent();
+    }
+}
