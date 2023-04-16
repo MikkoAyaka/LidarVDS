b19bb52c791f6eadabc3446062369bdfa42f4252
MikkoAyaka <3401286177@qq.com>
2023-04-16 11:04:25 +0800

    完成 Settings 页面模板
    Signed-off-by: MikkoAyaka <3401286177@qq.com>

---
+++ LidarVDS/Pages/Settings/PageSettings.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace LidarVDS.Pages.Settings;
+
+public partial class PageSettings : Page
+{
+    public PageSettings()
+    {
+        InitializeComponent();
+    }
+}
