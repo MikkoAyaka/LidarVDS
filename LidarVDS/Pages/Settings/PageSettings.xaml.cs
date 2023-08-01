87923a3971a8112a9af3409644bbcae979941cef
Conquer_CN <2832035049@qq.com>
2023-08-01 19:04:29 +0800

    2023/8/1 设置页面功能设计，页面美化未完成

--- LidarVDS/Pages/Settings/PageSettings.xaml.cs
+++ LidarVDS/Pages/Settings/PageSettings.xaml.cs
@@ -1,7 +1,5 @@
 using System.Windows.Controls;
 using LidarVDS.Pages.Settings.SettingPages.ConstantPage;
-using LidarVDS.Pages.Settings.SettingPages.VariablePage;
-using LidarVDS.Pages.Settings.SettingPages.Sys_Page;
 using LidarVDS.Pages.Settings.SettingPages.OtherPage;
 
 namespace LidarVDS.Pages.Settings;
@@ -17,14 +15,6 @@ public partial class PageSettings : Page
     {
         SettingFrame.Navigate(ConstantService.GetPage());
     } 
-    private void Variable_OnClick(object sender, System.Windows.RoutedEventArgs e)
-    {
-        SettingFrame.Navigate(VariableService.GetPage());
-    }
-    private void System_OnClick(object sender, System.Windows.RoutedEventArgs e)
-    {
-        SettingFrame.Navigate(Sys_Service.GetPage());
-    }
     private void Others_OnClick(object sender, System.Windows.RoutedEventArgs e)
     {
         SettingFrame.Navigate(OtherService.GetPage());
---
+++ LidarVDS/Pages/Settings/SettingPages/ConstantPage/Constant.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System.IO;
+using YamlDotNet.Serialization;
+using System.Collections.Generic;
+
+namespace LidarVDS.Pages.Settings.SettingPages.ConstantPage
+{
+    /// <summary>
+    /// Constant.xaml 的交互逻辑
+    /// </summary>
+
+    public partial class Constant : Page
+    {
+        private Dictionary<string, string> yamlData;//定义字典缓存数据
+        private string filePath = "D:\\LidarVDS\\Main\\LidarVDS\\LidarVDS\\bin\\Debug\\net6.0-windows\\Data.yaml";//获取文件位置
+
+        public Constant()
+        {
+            InitializeComponent();
+            yamlData = new Dictionary<string, string>();//声明字典
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            // 获取文本框中的输入数据
+            string input1 = Atmospheric_Transmissivity_Data.Text;
+            string input2 = Background_Noise_Data.Text;
+
+            // 将数据存入字典
+            yamlData["ATD"] = input1;
+            yamlData["BND"] = input2;
+
+            // 将数据写入YAML文件
+            var serializer = new SerializerBuilder().Build();
+            string yamlContent = serializer.Serialize(yamlData);
+            File.WriteAllText(filePath, yamlContent);
+        }
+
+        //读取.yaml文件的内容
+        public string ReadYamlFile(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        
+
+        //刷新按钮
+        private void Atmospheric_Transmissivity_Refresh(object sender, RoutedEventArgs e)
+        {
+            string filePath = "D:\\LidarVDS\\Main\\LidarVDS\\LidarVDS\\bin\\Debug\\net6.0-windows\\Data.yaml";
+            string yamlContent = File.ReadAllText(filePath);
+
+            var deserializer = new DeserializerBuilder().Build();
+            var yamlObject = deserializer.Deserialize<Dictionary<string, object>>(yamlContent);
+
+            // 获取指定键的值并显示在文本框中
+            if (yamlObject.ContainsKey("ATD"))
+            {
+                var value = yamlObject["ATD"].ToString();
+                Atmospheric_Transmissivity_Data.Text = value;
+            }
+        }
+
+        //刷新按钮
+        private void Background_Noise_Refresh(object sender, RoutedEventArgs e)
+        {
+            string filePath = "D:\\LidarVDS\\Main\\LidarVDS\\LidarVDS\\bin\\Debug\\net6.0-windows\\Data.yaml";
+            string yamlContent = File.ReadAllText(filePath);
+
+            var deserializer = new DeserializerBuilder().Build();
+            var yamlObject = deserializer.Deserialize<Dictionary<string, object>>(yamlContent);
+
+            // 获取指定键的值并显示在文本框中
+            if (yamlObject.ContainsKey("BND"))
+            {
+                var value = yamlObject["BND"].ToString();
+                Background_Noise_Data.Text = value;
+            }
+        }
+    }
+}
