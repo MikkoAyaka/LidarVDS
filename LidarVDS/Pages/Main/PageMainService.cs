b7d826fedc3a05545ab8e5a1ec6f32bf0eff0f7d
MikkoAyaka <3401286177@qq.com>
2023-08-27 11:23:03 +0800

    修复了公告图片无法显示的问题 将公告文本整理到代码中进行动态设置

--- LidarVDS/Pages/Main/PageMainService.cs
+++ LidarVDS/Pages/Main/PageMainService.cs
@@ -30,4 +30,9 @@ public class PageMainService
         //TODO 从历史操作记录中获取最近几次操作的信息摘要
         return new List<string>(){"2023/4/10 8:01 保存 激光雷达配置文件1","2023/4/10 8:00 编辑 激光雷达配置文件1","2023/4/10 7:59 创建 激光雷达配置文件1"};
     }
+
+    public List<string> GetAnnouncements()
+    {
+        return new List<string>() { "公告第一行", "公告第二行", "公告第三行" };
+    }
 }
