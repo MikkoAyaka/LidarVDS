6ee82af727c4f71ad78135ba65431f8a0d9e7c40
MikkoAyaka <3401286177@qq.com>
2023-08-30 23:32:03 +0800

    新建目录 Components 用于存放自定义组件 创建 ArgumentsTable 代码文件

---
+++ LidarVDS/Components/ArgumentsTable.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace LidarVDS.Components;
+
+public partial class ArgumentsTable : UserControl
+{
+    public ArgumentsTable()
+    {
+        InitializeComponent();
+    }
+}
