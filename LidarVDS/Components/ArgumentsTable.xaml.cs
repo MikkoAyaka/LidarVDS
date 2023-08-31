c6f58d1ecbfae4d99f3fbbf0f8d710c2656869c2
MikkoAyaka <3401286177@qq.com>
2023-08-31 13:38:01 +0800

    完成参数列表组件的初步封装

--- LidarVDS/Components/ArgumentsTable.xaml.cs
+++ LidarVDS/Components/ArgumentsTable.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using TextBox = HandyControl.Controls.TextBox;
 
 namespace LidarVDS.Components;
 
@@ -8,4 +13,25 @@ public partial class ArgumentsTable : UserControl
     {
         InitializeComponent();
     }
+
+    /**
+     * 显示环境参数列表
+     */
+    private void EnvironmentArguments(object sender, RoutedEventArgs e)
+    {
+        StackPanelContext.Children.Clear();
+        var li = new List<string>(){"PM2.5","PM10","AQI","相对风速","湿度","气压"};
+        foreach (var str in li)
+        {
+            TextBox textBox = new TextBox();
+            textBox.Text = str;
+            textBox.IsEnabled = false;
+            StackPanelContext.Children.Add(textBox);
+        }
+    }
+
+    /**
+     * 是否可以编辑，默认否
+     */
+    public bool Editable { get; set; } = false;
 }
