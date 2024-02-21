2a7f041fc770de682fb2d72385b4806b96c7cbe0
Conquer_CN <2832035049@qq.com>
2024-02-21 14:38:08 +0800

    9/20 设置界面优化

--- LidarVDS/Pages/Settings/SettingPages/ParameterPage/Parameter.xaml.cs
+++ LidarVDS/Pages/Settings/SettingPages/ParameterPage/Parameter.xaml.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using YamlDotNet.Serialization;
 using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
 
 namespace LidarVDS.Pages.Settings.SettingPages.ParameterPage
 {
@@ -9,59 +11,73 @@ namespace LidarVDS.Pages.Settings.SettingPages.ParameterPage
     {
         private Dictionary<string, string> _yamlData;//定义字典缓存数据
         
-        private string _filePath = "D:/LidarVDS/LidarVDS/LidarVDS/Pages/Settings/Data.yaml";//获取文件位置
-        
+        private string _filePath = @"../../Debug/net6.0-windows/Settings.yaml";//获取文件位置
+            //"D:\LidarVDS\LidarVDS\LidarVDS\bin\Debug\net6.0-windows\Settings.yaml"
         public Parameter()//初始化页面
         {
             InitializeComponent();
-            _yamlData = new Dictionary<string, string>();//声明字典
+            //LoadSavedData();
+            //_yamlData = new Dictionary<string, string>();//声明字典
         }
 
         private void Save(object sender, RoutedEventArgs e)//保存按钮
         {
-            // // 获取文本框中的输入数据
-            // string input1 = WindSpeed.Text;
-            // string input2 = Humidity.Text;
-            // string input3 = Pressure.Text;
-            //
-            // // 将数据存入字典
-            // _yamlData["WindSpeed"] = input1;
-            // _yamlData["Humidity"] = input2;
-            // _yamlData["Pressure"] = input3;
+            // 获取选择的内容
+            string selectedColor = ((ComboBoxItem)Color.SelectedItem).Content.ToString();//主体颜色
+            string selectedSize = ((ComboBoxItem)Size.SelectedItem).Content.ToString();//字体大小
 
+            // 整合数据
+            var settings = new
+            {
+                MainColor = selectedColor,
+                FontSize = selectedSize,
+                // 可根据需要添加更多选项
+            };
             // 将数据写入YAML文件
-            var serializer = new SerializerBuilder().Build();
-            string yamlContent = serializer.Serialize(_yamlData);
-            File.WriteAllText(_filePath, yamlContent);
+            var serializer = new SerializerBuilder().Build();//新建串行器
+            
+            string yamlContent = serializer.Serialize(settings);
+            
+            File.WriteAllText(_filePath, yamlContent, Encoding.UTF8);//写入文件
         }
 
         //刷新按钮
         private void Refresh(object sender, RoutedEventArgs e)
         {
-            string yamlContent = File.ReadAllText(_filePath);
+            // 刷新按钮点击事件，重新加载保存的数据
+            LoadSavedData();
+        }
+        
+        //加载内容
+        private void LoadSavedData()
+        {
+            if (File.Exists(_filePath))
+            {
+                // 读取保存的数据
+                string yamlContent = File.ReadAllText(_filePath);
 
-            var deserializer = new DeserializerBuilder().Build();
-            var yamlObject = deserializer.Deserialize<Dictionary<string, object>>(yamlContent);
+                // 反序列化YAML为对象
+                var deserializer = new DeserializerBuilder().Build();
+                var settings = deserializer.Deserialize<dynamic>(yamlContent);
 
-            // 获取指定键的值并显示在文本框中
-            if (yamlObject.TryGetValue("WindSpeed", out var value1))
-            {
-                var value = value1.ToString();
-                // WindSpeed.Text = value;
+                // 设置下拉框的选定值
+                SetValue(Color, settings.MainColor);
+                SetValue(Size, settings.FontSize);
+                // 根据需要设置更多下拉框的选定值
             }
-            
-            if (yamlObject.TryGetValue("Humidity", out var value2))
+        }
+
+        private void SetValue(ComboBox comboBox, string value)
+        {
+            // 设置下拉框的选定值
+            foreach (ComboBoxItem item in comboBox.Items)
             {
-                var value = value2.ToString();
-                // Humidity.Text = value;
+                if (item.Content.ToString() == value)
+                {
+                    comboBox.SelectedItem = item;
+                    break;
+                }
             }
-            
-            if (yamlObject.TryGetValue("Pressure", out var value3))
-            {
-                var value = value3.ToString();
-                // Pressure.Text = value;
-            }
-                
         }
     }
 }
