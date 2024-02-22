1fedfd41e11344555f3f38ec93c5e7fe9123ee6c
HaogerField <3447688953@qq.com>
2024-02-22 17:32:57 +0800

    统计图曲线绘图算法修改

--- LidarVDS/Pages/Analysis/PageAnalysis.xaml.cs
+++ LidarVDS/Pages/Analysis/PageAnalysis.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using LidarVDS.Utils;
 using Microsoft.Research.DynamicDataDisplay.DataSources;
+using Point = System.Windows.Point;
 
 namespace LidarVDS.Pages.Analysis;
 
@@ -13,7 +15,7 @@ public partial class PageAnalysis : Page
     internal PageAnalysis()
     {
         InitializeComponent();
-        // Frame.NavigationService.LoadCompleted += NavigationService_LoadCompleted;
+        Loaded += new RoutedEventHandler(MainWindow_Loaded);
     }
 
     
@@ -21,30 +23,32 @@ public partial class PageAnalysis : Page
     void MainWindow_Loaded(object sender, RoutedEventArgs e)
     {
         //给曲线图绑定数据源
-        line_black.DataSource = CreateDataSource(7000,0.0005);
-        // line_blue.DataSource = CreateSineDataSource(3.0); ;
-        // line_red.DataSource = CreateSineDataSource(5.0); ;
+        line_black.DataSource = CreateDataSource(7000, 0.0005);
     
         // Force evertyhing plotted to be visible
         plotter.FitToView();
     }
+    
     /**
      * viewDistance 能见度 单位米
      * scatteringCoefficient 大气颗粒散射系数 单位每米，取值范围在 10^5/m ~ 10^6/m
      */
-    private IPointDataSource CreateDataSource(int viewDistance,double scatteringValue)
+    private IPointDataSource CreateDataSource(double viewDistance,double scatteringValue)
     {
         const int maxLen = 5000;
-        Point[] pts = new Point[maxLen];
-        // Dictionary<Double,Double> dataMap = Computer.MainAlg( viewDistance, scatteringValue,maxLen);
-        // for (int i = 1; i < maxLen; i++)
-        // {
-        //     pts[i] = new Point(i, dataMap[i]);
-        // }
-        var ds = new EnumerableDataSource<Point>(pts);
-        ds.SetXYMapping(pt => pt);
-        return ds;
+        List<Point> dataPoints = new List<Point>();
+        for (int i = 0; i <= maxLen; i++)
+        {
+            double x = i;
+            double y = Computer.MainAlg(i, viewDistance, scatteringValue);
+            dataPoints.Add(new Point(x,y));
+        }
+
+        var dataSource = new EnumerableDataSource<Point>(dataPoints);
+        dataSource.SetXYMapping(pt => pt);
+        return dataSource;
     }
+    
 
     
     private void HistoryPage_Onclick(object sender, System.Windows.RoutedEventArgs e)
