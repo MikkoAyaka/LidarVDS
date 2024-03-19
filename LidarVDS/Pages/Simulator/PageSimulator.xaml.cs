42883611709ed28a72bb7d55a09c4d90074c25e1
MikkoAyaka <3401286177@qq.com>
2024-03-19 19:06:34 +0800

    PageSimulator 改为延迟初始化

--- LidarVDS/Pages/Simulator/PageSimulator.xaml.cs
+++ LidarVDS/Pages/Simulator/PageSimulator.xaml.cs
@@ -1,57 +1,44 @@
 using System;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using LidarVDS.Maths;
-using LidarVDS.Utils;
-using Microsoft.Research.DynamicDataDisplay.DataSources;
 
 namespace LidarVDS.Pages.Simulator;
 
 public partial class PageSimulator : Page
-{
+{ 
     [Obsolete("请通过相应Service类获取单例实例")]
     public PageSimulator()
     {
         InitializeComponent();
-        
-        Loaded += new RoutedEventHandler(MainWindow_Loaded);
-
-        ComboBox.Items.Add("斜率法");
-        ComboBox.Items.Add("改进的斜率法");
-        ComboBox.Items.Add("改进的 Fernald 算法");
-        ComboBox.SelectedIndex = 0;
+        LateInit();
+        // ComboBox.Items.Add("斜率法");
+        // ComboBox.Items.Add("改进的斜率法");
+        // ComboBox.Items.Add("改进的 Fernald 算法");
+        // ComboBox.SelectedIndex = 0;
     }
 
-    private void slider_PM25_ValueChanged(object sender, System.Windows.RoutedPropertyChangedEventArgs<double> e)
+    private async void LateInit()
     {
-        //var width = slider_PM25
-    }
-
-    void MainWindow_Loaded(object sender, RoutedEventArgs e)
-    {
-        //给曲线图绑定数据源
-        line_black.DataSource = CreateDataSource();
-        // line_blue.DataSource = CreateSineDataSource(3.0); ;
-        // line_red.DataSource = CreateSineDataSource(5.0); ;
-
-        // Force evertyhing plotted to be visible
-        plotter.FitToView();
-    }
-    /**
-     * viewDistance 能见度 单位米
-     * scatteringCoefficient 大气颗粒散射系数 单位每米，取值范围在 10^5/m ~ 10^6/m
-     */
-    private IPointDataSource CreateDataSource()
-    {
-        const int maxLen = 5000;
-        Point[] pts = new Point[maxLen];
-        for (int i = 1; i < maxLen; i++)
+        while (!IsLoaded)
         {
-            pts[i] = new Point(i, EchoParticleGenerator.Instance.Accept(i));
+            await Task.Delay(1);
         }
-        var ds = new EnumerableDataSource<Point>(pts);
-        ds.SetXYMapping(pt => pt);
-        return ds;
+        ViewBtn1.IsChecked = true;
+    }
+
+    private void EPG_Selected(object sender, RoutedEventArgs e)
+    {
+        PageSimulatorService.GetInstance().ChangeDataSource_EPG();
+    }
+
+    private void GOF_Selected(object sender, RoutedEventArgs e)
+    {
+        PageSimulatorService.GetInstance().ChangeDataSource_GOF();
+    }
+
+    private void AEC_Selected(object sender, RoutedEventArgs e)
+    {
+        PageSimulatorService.GetInstance().ChangeDataSource_AEC();
     }
 }
--- LidarVDS/Pages/Simulator/PageSimulatorService.cs
+++ LidarVDS/Pages/Simulator/PageSimulatorService.cs
@@ -1,3 +1,9 @@
+using System.Diagnostics;
+using System.Windows;
+using LidarVDS.Maths;
+using Microsoft.Research.DynamicDataDisplay;
+using Microsoft.Research.DynamicDataDisplay.DataSources;
+
 namespace LidarVDS.Pages.Simulator;
 
 public class PageSimulatorService
@@ -15,5 +21,65 @@ public class PageSimulatorService
     public static PageSimulatorService GetInstance()
     {
         return InstanceHolder.Instance;
+    }
+
+    /**
+     * 将数据源更新为 几何重叠因子 相关图像
+     */
+    public void ChangeDataSource_GOF()
+    {
+        
+    }
+    /**
+     * 将数据源更新为 大气消光系数 相关图像
+     */
+    public void ChangeDataSource_AEC()
+    {
+        
+    }
+    /**
+     * 将数据源更新为 回波粒子信号 相关图像
+     */
+    public void ChangeDataSource_EPG()
+    {
+        //给曲线图绑定数据源
+        InstanceHolder.Page.LineBlack.DataSource = CreateDataSource_EPG_WithoutNoice();
+        InstanceHolder.Page.LineBlack.Description = new PenDescription("回波粒子信号 不含噪");
+        InstanceHolder.Page.LineBlue.DataSource = CreateDataSource_EPG_WithNoice();
+        InstanceHolder.Page.LineBlue.Description = new PenDescription("回波粒子信号 含噪");
+        // 渲染
+        InstanceHolder.Page.plotter.FitToView();
+    }
+    /**
+     * 不含噪的回波粒子信号图像产生函数
+     */
+    private IPointDataSource CreateDataSource_EPG_WithoutNoice()
+    {
+        const int maxLen = 5000;
+        Point[] pts = new Point[maxLen];
+        for (int i = 1; i < maxLen; i++)
+        {
+            pts[i] = new Point(i, EchoParticleGenerator.Instance.Accept(i));
+        }
+        var ds = new EnumerableDataSource<Point>(pts);
+        ds.SetXYMapping(pt => pt);
+        return ds;
+    }
+    /**
+     * 含噪的回波粒子信号图像产生函数
+     */
+    private IPointDataSource CreateDataSource_EPG_WithNoice()
+    {
+        const int maxLen = 5000;
+        Point[] pts = new Point[maxLen];
+        for (int i = 1; i < maxLen; i++)
+        {
+            // TODO 这里应该改为这样：
+            // pts[i] = new Point(i, EchoParticleGenerator.Instance.Accept(i) + GuassianNoice.Instance.Accept(i));
+            pts[i] = new Point(i,0);
+        }
+        var ds = new EnumerableDataSource<Point>(pts);
+        ds.SetXYMapping(pt => pt);
+        return ds;
     }
 }
